785d12158f1dcd7080941879c340c1b641994fb0
Yoojin Lee <yoojinLee.dev@gmail.com>
2020-11-19 11:47:29 +0900

    feat: Add bbs table and MVC
    게시판 기능 구현을 위한 테이블 생성 및 MVC 생성

---
+++ PortFolio/src/net/bbs/action/BBSFrontController.java
@@ -0,0 +1,49 @@
+package net.bbs.action;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class BBSFrontController extends HttpServlet implements javax.servlet.Servlet {
+	protected void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		String RequestURI = request.getRequestURI();
+		String ContextPath = request.getContextPath();
+		String command = RequestURI.substring(ContextPath.length());
+		
+		System.out.println(RequestURI);
+		System.out.println(ContextPath);
+		System.out.println(command);
+		
+		ActionForward forward = null;
+		Action action = null;
+		
+		if(command.equals("")) {
+			action = null();
+			try {
+				forward = action.execute(request, response);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		if(forward != null) {
+			if(forward.isRedirect()) {
+				response.sendRedirect(forward.getPath());
+			} else {
+				RequestDispatcher dispatcher = request.getRequestDispatcher(forward.getPath());
+				dispatcher.forward(request, response);
+			}
+		}
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		doProcess(request, response);
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		doProcess(request, response);
+	}
+}
---
+++ PortFolio/src/net/bbs/db/BBSDTO.java
@@ -0,0 +1,5 @@
+package net.bbs.db;
+
+public class BBSDTO {
+
+}
